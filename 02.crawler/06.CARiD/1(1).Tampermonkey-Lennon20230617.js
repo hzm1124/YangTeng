82dacc0abdb76505b95da69e08c4f203489faaaf
lennon1124 <1210882202@qq.com>
2023-06-19 11:26:04 +0800

    update crawler

--- 02.crawler/06.CARiD/1(1).Tampermonkey-Lennon20230617.js
+++ 02.crawler/06.CARiD/1(1).Tampermonkey-Lennon20230617.js
@@ -1,15 +1,11 @@
 // ==UserScript==
-// @name         eBay爬虫-sch
+// @name         CARiD爬虫-sch
 // @namespace    https://github.com/lennon1124/YangTeng
-// @version      2023.06.13
+// @version      2023.06.17
 // @description  爬虫系列
 // @author       Lennon
-// @match        *://www.ebay.com/sch/*
-// @match        *://www.ebay.de/sch/*
-// @match        *://www.ebay.co.uk/sch/*
-// @match        *://www.ebay.com.au/sch/*
-// @match        *://www.ebay.ca/sch/*
-// @icon         https://www.ebay.com/favicon.ico
+// @match        *://www.carid.com/search/*
+// @icon         https://www.carid.com/domain-specific/carid/favicon.ico
 // @grant        none
 // ==/UserScript==
 
@@ -21,14 +17,11 @@
         window.scrollTo(0, document.body.scrollHeight);
 
         // 输出内容
-        let list_item = document.getElementsByClassName('s-item__image-section');
+        let list_part = document.getElementsByClassName('js-main-prod-list prod_lst prod-lst-colors-show js-use-extended-hover js-prod-list-ga-analytics-sent')[0].getElementsByClassName('lst-info-left-part');
         let array_content = new Array();
-        let item_url;
-        for(let i=1; i<list_item.length; i++) {
-            array_content[i-1] = {};
-            item_url = list_item[i].getElementsByTagName('div')[0].getElementsByTagName('a')[0].getAttribute('href');
-            array_content[i-1]['Item_Number'] = item_url.split('?')[0].substr(-12, 12);
-            array_content[i-1]['Url'] = item_url;
+        for(let i=0; i<list_part.length; i++) {
+            array_content[i] = {};
+            array_content[i]['Url'] = 'https://www.carid.com' + list_part[i].getElementsByTagName('a')[0].getAttribute('href');
         }
 
         // json
@@ -45,6 +38,8 @@
         URL.revokeObjectURL(blob_url);
 
         // next
-        document.getElementsByClassName('pagination__next icon-link')[0].click();
+        if(document.getElementsByClassName('pagination-next')[0].getAttribute('href') != null) {
+            window.location.href = 'https://www.carid.com' + document.getElementsByClassName('pagination-next')[0].getAttribute('href');
+        }
     }
 })();
