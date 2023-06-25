362e7619e1a95022b37804db39dfc7cc061bf770
lennon1124 <1210882202@qq.com>
2023-06-25 15:28:13 +0800

    update crawler

--- 02.crawler/08.SpectraPremium/1(1).Tampermonkey-Lennon20230619.js
+++ 02.crawler/08.SpectraPremium/1(1).Tampermonkey-Lennon20230619.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         CARiD爬虫-sch
+// @name         SpectraPremium爬虫-sch
 // @namespace    https://github.com/lennon1124/YangTeng
-// @version      2023.06.17
+// @version      2023.06.19
 // @description  爬虫
 // @author       Lennon
-// @match        *://www.carid.com/search/*
-// @icon         https://www.carid.com/domain-specific/carid/favicon.ico
+// @match        *://ecat.spectrapremium.com/en/parts?*
+// @icon         https://ecat.spectrapremium.com/safari-pinned-tab.svg
 // @grant        none
 // ==/UserScript==
 
@@ -17,11 +17,12 @@
         window.scrollTo(0, document.body.scrollHeight);
 
         // 输出内容
-        let list_part = document.getElementsByClassName('js-main-prod-list prod_lst prod-lst-colors-show js-use-extended-hover js-prod-list-ga-analytics-sent')[0].getElementsByClassName('lst-info-left-part');
+        let list_part_number = document.getElementsByClassName('tuile-produit__code-produit');
         let array_content = new Array();
-        for(let i=0; i<list_part.length; i++) {
+        for(let i=0; i<list_part_number.length; i++) {
             array_content[i] = {};
-            array_content[i]['Url'] = 'https://www.carid.com' + list_part[i].getElementsByTagName('a')[0].getAttribute('href');
+            array_content[i]['Part_Number'] = list_part_number[i].innerText;
+            array_content[i]['Url'] = 'https://ecat.spectrapremium.com' + list_part_number[i].getElementsByTagName('a')[0].getAttribute('href');
         }
 
         // json
@@ -38,8 +39,6 @@
         URL.revokeObjectURL(blob_url);
 
         // next
-        if(document.getElementsByClassName('pagination-next')[0].getAttribute('href') != null) {
-            window.location.href = 'https://www.carid.com' + document.getElementsByClassName('pagination-next')[0].getAttribute('href');
-        }
+        document.getElementsByClassName('paginator__next')[0].getElementsByTagName('a')[0].click();
     }
 })();
