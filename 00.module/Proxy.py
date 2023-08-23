54192156f6dd300537b047150df7ea24e8336fea
lennon1124 <1210882202@qq.com>
2023-08-23 08:41:09 +0800

    update module

---
+++ 00.module/Proxy.py
@@ -0,0 +1,13 @@
+import random
+
+username = 't19182064816778'
+password = 'm0my10ty'
+tunnel_host = 'f290.kdltps.com'
+tunnel_port = '15818'
+
+def get_Proxy_requests():
+    return {'http': 'http://' + username + ':' + password + '@' + tunnel_host + ':' + tunnel_port,
+            'https': 'http://' + username + ':' + password + '@' + tunnel_host + ':' + tunnel_port};
+
+def get_Proxy_Selenium():
+    return '--proxy-server=http://' + tunnel_host + ':' + tunnel_port;
