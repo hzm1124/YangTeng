75322906529a9bc57035d3d6c3b3354a7fb2bf48
lennon1124 <1210882202@qq.com>
2023-09-08 10:01:59 +0800

    update crawler

--- 02.crawler/02.Dorman/1(1).Menu_JS-Lennon20230908.js
+++ 02.crawler/02.Dorman/1(1).Menu_JS-Lennon20230908.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dorman爬虫-sch
 // @namespace    https://github.com/lennon1124/YangTeng
-// @version      2023.08.23
+// @version      2023.09.08
 // @description  爬虫
 // @author       Lennon
 // @match        *://www.dormanproducts.com/gsearch.aspx?*
@@ -15,14 +15,15 @@
     // Your code here...
     window.onload = function() {
         window.scrollTo(0, document.body.scrollHeight);
+        console.log('爬虫启动...');
 
-        let list_part = document.getElementsByClassName('item-name');
+        let list_part = document.querySelectorAll('span[class="item-name"]');
         let array_part_number = new Array();
         for(let i=0; i<list_part.length; i++) {
             array_part_number.push(list_part[i].innerText);
         }
 
-        let list_description = document.getElementsByClassName('searchItems-info');
+        let list_description = document.querySelectorAll('div[class="searchItems-info"]');
         let array_part_type = new Array();
         let array_application_summary = new Array();
         for(let i=0; i<list_description.length; i++) {
@@ -36,13 +37,13 @@
             }
         }
 
-        let list_url = document.getElementsByClassName('searchItems-img');
+        let list_url = document.querySelectorAll('div[class="searchItems-img"]');
         let array_url = new Array();
         for(let i=0; i<list_url.length; i++) {
-            array_url.push('https://www.dormanproducts.com/' + list_url[i].getElementsByTagName('a')[0].getAttribute('href'));
+            array_url.push('https://www.dormanproducts.com/' + list_url[i].querySelector('a').getAttribute('href'));
         }
 
-        // 输出内容
+        // array
         let array_content = new Array();
         for(let i=0; i<array_part_number.length; i++) {
             array_content[i] = {};
@@ -51,21 +52,23 @@
             array_content[i]['Application_Summary'] = array_application_summary[i];
             array_content[i]['Url'] = array_url[i];
         }
+        console.log('数量：' + array_content.length);
 
         // json
         let json_content = JSON.stringify(array_content);
-        console.log(json_content);
+        console.log('JSON格式');
 
         // blob
         let blob_data = new Blob([json_content], {type: 'text/plain'});
         let blob_url = URL.createObjectURL(blob_data);
         let blob_a = document.createElement('a');
         blob_a.href = blob_url;
-        blob_a.download = window.location.href.replace('http://', '').replace('https://', '') + '.txt';
+        blob_a.download = window.location.href.replace('https://', '').replace('http://', '') + '.txt';
         blob_a.click();
         URL.revokeObjectURL(blob_url);
+        console.log('搞定！');
 
         // next
-        document.getElementById('pagingBottom_nextButton').click();
+        document.querySelector('a[id="pagingBottom_nextButton"]').click();
     }
 })();
