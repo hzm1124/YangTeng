6ba62fa6a5081f1a575b90a1ae71a002e3e7f599
lennon1124 <1210882202@qq.com>
2023-09-08 10:51:17 +0800

    update crawler

--- 02.crawler/03.eBay/1(1).Menu_JS-Lennon20230908.js
+++ 02.crawler/03.eBay/1(1).Menu_JS-Lennon20230908.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         eBay爬虫-sch
 // @namespace    https://github.com/lennon1124/YangTeng
-// @version      2023.06.16
+// @version      2023.09.08
 // @description  爬虫
 // @author       Lennon
 // @match        *://www.ebay.com/sch/*
@@ -19,21 +19,24 @@
     // Your code here...
     window.onload = function() {
         window.scrollTo(0, document.body.scrollHeight);
+        console.log('爬虫启动...');
 
-        // 输出内容
-        let list_item = document.getElementsByClassName('s-item__image-section');
+        let list_item = document.querySelectorAll('div[class="s-item__image-section"]');
+
+        // array
         let array_content = new Array();
         let url;
         for(let i=1; i<list_item.length; i++) {
             array_content[i-1] = {};
-            url = list_item[i].getElementsByTagName('div')[0].getElementsByTagName('a')[0].getAttribute('href');
-            array_content[i-1]['Item_Number'] = url.split('?')[0].substr(-12, 12);
+            url = list_item[i].querySelector('div').querySelector('a').getAttribute('href');
             array_content[i-1]['Url'] = url;
+            array_content[i-1]['Item_Number'] = url.split('?')[0].substr(-12, 12);
         }
+        console.log('数量：' + array_content.length);
 
         // json
         let json_content = JSON.stringify(array_content);
-        console.log(json_content);
+        console.log('JSON格式');
 
         // blob
         let blob_data = new Blob([json_content], {type: 'text/plain'});
@@ -43,8 +46,9 @@
         blob_a.download = window.location.href.replace('http://', '').replace('https://', '') + '.txt';
         blob_a.click();
         URL.revokeObjectURL(blob_url);
+        console.log('搞定！');
 
         // next
-        document.getElementsByClassName('pagination__next icon-link')[0].click();
+        document.querySelector('a[class="pagination__next icon-link"]').click();
     }
 })();
