1e6da7ca06c6a252b461f79fb58c45213462ce3a
lennon1124 <1210882202@qq.com>
2023-09-11 08:27:12 +0800

    update crawler

--- 00.module/Proxy.py
+++ 00.module/Proxy.py
@@ -5,9 +5,9 @@ password = 'm0my10ty'
 tunnel_host = 'f290.kdltps.com'
 tunnel_port = '15818'
 
-def get_Proxy_requests():
+def get_Proxy_Requests():
     return {'http': 'http://' + username + ':' + password + '@' + tunnel_host + ':' + tunnel_port,
             'https': 'http://' + username + ':' + password + '@' + tunnel_host + ':' + tunnel_port};
 
-def get_Proxy_selenium():
+def get_Proxy_Selenium():
     return '--proxy-server=http://' + tunnel_host + ':' + tunnel_port;
