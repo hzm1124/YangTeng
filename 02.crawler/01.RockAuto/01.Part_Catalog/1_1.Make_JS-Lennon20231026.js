c4d1985b3bfc14d0503052ba073081d2114f202c
lennon1124 <1210882202@qq.com>
2023-11-27 13:40:23 +0800

    update crawler

---
+++ 02.crawler/01.RockAuto/01.Part_Catalog/1_1.Make_JS-Lennon20231026.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         RockAuto爬虫-Part_Catalog
+// @namespace    https://github.com/lennon1124/YangTeng
+// @version      2023.10.26
+// @description  Crawler
+// @author       Lennon
+// @match        *://www.rockauto.com/en/catalog/*
+// @icon         https://www.rockauto.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.onload = function() {
+        window.scrollTo(0, document.body.scrollHeight);
+        console.log('\n\n\n= = = = = = = = = = = = = = =');
+        console.log('Crawler Log 1: Start');
+
+        // array
+        let array_data = new Array();
+        let list_part = document.querySelectorAll('td.niconspace.ncollapsedicon>a');
+        for(let i=0; i<list_part.length; i++) {
+            array_data[i] = {};
+            array_data[i]['No.'] = i + 1;
+            array_data[i]['Url'] = 'https://www.rockauto.com' + list_part[i].getAttribute('href');
+        }
+        console.log('Crawler Log 2: Array');
+
+        // json
+        let json_data = JSON.stringify(array_data);
+        console.log('Crawler Log 3: Json');
+
+        // blob
+        let blob_data = new Blob([json_data], {type: 'text/plain'});
+        let blob_url = URL.createObjectURL(blob_data);
+        let blob_a = document.createElement('a');
+        blob_a.href = blob_url;
+        blob_a.download = window.location.href + '.txt';
+        blob_a.click();
+        URL.revokeObjectURL(blob_url);
+        console.log('Crawler Log 4: End');
+        console.log('= = = = = = = = = = = = = = =\n\n\n');
+    }
+})();
