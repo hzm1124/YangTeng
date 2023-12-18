957cb523f4f456464b6f23e80fa443acad9d7b2b
lennon1124 <1210882202@qq.com>
2023-12-18 15:42:27 +0800

    update crawler

--- 02.crawler/12.AutoteileDirekt/3_3.Pic_JS-Lennon20231216.js
+++ 02.crawler/12.AutoteileDirekt/3_3.Pic_JS-Lennon20231216.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         AutoteileDirekt爬虫-Pic
 // @namespace    https://github.com/lennon1124/YangTeng
-// @version      2023.09.12
-// @description  爬虫
+// @version      2023.12.16
+// @description  Crawler
 // @author       Lennon
 // @match        *://media.autoteiledirekt.de/*
 // @match        *://cdn.autoteiledirekt.de/*
@@ -13,7 +13,6 @@
 (function() {
     'use strict';
 
-    // Your code here...
     window.onload = function() {
         function sleep(time) {
             return(new Promise(function(resolve, reject) {
@@ -23,27 +22,28 @@
 
         window.scrollTo(0, document.body.scrollHeight);
         console.log('\n\n\n= = = = = = = = = = = = = = =');
-        console.log('Tampermonkey 1：爬虫启动');
+        console.log('Crawler Log 1: Start');
 
-        sleep(3000).then(function() {
-            let pic = document.querySelector('img');
-            console.log('Tampermonkey 2：图片获取成功');
+        sleep(2000).then(function() {
+            // img
+            let img = document.querySelector('img');
+            console.log('Crawler Log 2: Img');
 
             // canvas
             let canvas = document.createElement('canvas');
-            canvas.width = pic.width;
-            canvas.height = pic.height;
-            canvas.getContext('2d').drawImage(pic, 0, 0, canvas.width, canvas.height);
+            canvas.width = img.width;
+            canvas.height = img.height;
+            canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height);
+            console.log('Crawler Log 3: Canvas');
 
             // blob
             let blob_a = document.createElement('a');
             blob_a.href = canvas.toDataURL();
             blob_a.download = window.location.href + '.jpeg';
             blob_a.click();
-            console.log('Tampermonkey 3：爬虫结束');
-            console.log('= = = = = = = = = = = = = = =\n\n\n');
+            console.log('Crawler Log 4: Blob');
 
-            // next
+            // index
             let list_param = window.location.href.split('?')[1].split('&');
             let param_index;
             for(let i=0; i<list_param.length; i++) {
@@ -51,8 +51,14 @@
                     param_index = list_param[i].split('index=')[1];
                 }
             }
-            console.log('下一个index：' + (Number(param_index) + 1));
-            window.location.href = 'http://localhost:8080/crawler/12.AutoteileDirekt_2.html?index=' + (Number(param_index) + 1);
+            console.log('Crawler Log 5: Index is ' + param_index);
+            console.log('Crawler Log 6: Next is ' + (Number(param_index) + 1));
+
+            //url
+            let url = 'http://localhost:8080/crawler/12.AutoteileDirekt_3.html?index=' + (Number(param_index) + 1);
+            console.log('Crawler Log 7: Go to ' + url);
+            console.log('= = = = = = = = = = = = = = =\n\n\n');
+            window.location.href = url;
         })
     }
 })();
