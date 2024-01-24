33f147351f2d4a79a13be9c2393f55139b51666c
lennon1124 <1210882202@qq.com>
2024-01-24 14:11:59 +0800

    update crawler

--- 00.module/Proxy.py
+++ 00.module/Proxy.py
@@ -10,35 +10,38 @@ def get_Proxy_Requests():
     return {'http': 'http://' + username + ':' + password + '@' + tunnel_host + ':' + tunnel_port,
             'https': 'http://' + username + ':' + password + '@' + tunnel_host + ':' + tunnel_port}
 
-def get_Proxy_Selenium():
-    manifest_json = '''{"version": "1.0.0",
- "manifest_version": 2,
- "name": "Chrome Proxy",
- "permissions": ["proxy",
-                 "tabs",
-                 "unlimitedStorage",
-                 "storage",
-                 "<all_urls>",
-                 "webRequest",
-                 "webRequestBlocking"],
- "background": {"scripts": ["background.js"]},
- "minimum_chrome_version": "22.0.0"}'''
+def get_Proxy_Selenium(coroutine):
+    if coroutine == 'coroutine':
+        return './proxy_selenium.zip'
+    else:
+        manifest_json = '''{"version": "1.0.0",
+     "manifest_version": 2,
+     "name": "Chrome Proxy",
+     "permissions": ["proxy",
+                     "tabs",
+                     "unlimitedStorage",
+                     "storage",
+                     "<all_urls>",
+                     "webRequest",
+                     "webRequestBlocking"],
+     "background": {"scripts": ["background.js"]},
+     "minimum_chrome_version": "22.0.0"}'''
 
-    background_js = '''var config = {mode: "fixed_servers",
-              rules: {singleProxy: {scheme: "http",
-                                    host: "''' + tunnel_host + '''",
-                                    port: parseInt(''' + tunnel_port + ''')},
-                      bypassList: ["foobar.com"]}};
-chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
-function callbackFn(details) {
-    return {authCredentials: {username: "''' + username + '''",
-                              password: "''' + password + '''"}};}
-chrome.webRequest.onAuthRequired.addListener(callbackFn,
-                                             {urls: ["<all_urls>"]},
-                                             ['blocking']);'''
+        background_js = '''var config = {mode: "fixed_servers",
+                  rules: {singleProxy: {scheme: "http",
+                                        host: "''' + tunnel_host + '''",
+                                        port: parseInt(''' + tunnel_port + ''')},
+                          bypassList: ["foobar.com"]}};
+    chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+    function callbackFn(details) {
+        return {authCredentials: {username: "''' + username + '''",
+                                  password: "''' + password + '''"}};}
+    chrome.webRequest.onAuthRequired.addListener(callbackFn,
+                                                 {urls: ["<all_urls>"]},
+                                                 ['blocking']);'''
 
-    with zipfile.ZipFile('proxy_selenium.zip', 'w') as zp:
-        zp.writestr("manifest.json", manifest_json)
-        zp.writestr("background.js", background_js)
+        with zipfile.ZipFile('proxy_selenium.zip', 'w') as zp:
+            zp.writestr("manifest.json", manifest_json)
+            zp.writestr("background.js", background_js)
 
-    return './proxy_selenium.zip'
+        return './proxy_selenium.zip'
--- 02.crawler/03.eBay/1.Search/1_1.Menu_JS-Lennon20240124.js
+++ 02.crawler/03.eBay/1.Search/1_1.Menu_JS-Lennon20240124.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @name         eBay爬虫-Menu
+// @name         eBay爬虫-Search
 // @namespace    https://github.com/lennon1124/YangTeng
-// @version      2023.12.13
+// @version      2024.01.24
 // @description  Crawler
 // @author       Lennon
 // @match        *://www.ebay.com/*
+// @match        *://www.ebay.de/*
+// @match        *://www.ebay.com.au/*
 // @icon         https://www.ebay.com/favicon.ico
 // @grant        none
 // ==/UserScript==
@@ -22,7 +24,12 @@
         let list_part = document.querySelectorAll('span.s-item__item-id.s-item__itemID');
         for(let i=0; i<list_part.length; i++) {
             array_data[i] = {};
-            array_data[i]['Item_Number'] = list_part[i].innerText.replace('Item:', '').trim();
+            if(window.location.href.indexOf('www.ebay.de/') != -1) {
+                array_data[i]['Item_Number'] = list_part[i].innerText.replace('Artikel:', '').trim();
+            }
+            else {
+                array_data[i]['Item_Number'] = list_part[i].innerText.replace('Item:', '').trim();
+            }
             array_data[i]['No.'] = i + 1;
         }
         console.log('Crawler Log 2: Array');
