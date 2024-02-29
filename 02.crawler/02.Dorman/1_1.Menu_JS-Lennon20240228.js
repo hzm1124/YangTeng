6fe869772ee2998af94664cfc2f7ac7140b8abe7
lennon1124 <1210882202@qq.com>
2024-02-29 15:46:06 +0800

    update crawler

---
+++ 02.crawler/02.Dorman/1_1.Menu_JS-Lennon20240228.js
@@ -0,0 +1,23 @@
+let list_url = ["https://www.dormanproducts.com/gsearch.aspx?type=keyword&origin=keyword&parttype=Fuel%2520Filler%2520Neck&start=0&num=100","https://www.dormanproducts.com/gsearch.aspx?type=keyword&origin=keyword&parttype=Fuel%2520Filler%2520Neck&start=100&num=100","https://www.dormanproducts.com/gsearch.aspx?type=keyword&origin=keyword&parttype=Fuel%2520Filler%2520Neck&start=200&num=100","https://www.dormanproducts.com/gsearch.aspx?type=keyword&origin=keyword&parttype=Fuel%2520Filler%2520Neck&start=300&num=100"];
+console.log('总数量：' + String(list_url.length));
+
+for(let i=0; i<list_url.length; i++) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', list_url[i], true);
+    xhr.onload = function() {
+        if(xhr.status == 200 && xhr.readyState == 4) {
+            let blob_data = new Blob([xhr.response], {type: 'text/plain'});
+            let blob_url = URL.createObjectURL(blob_data);
+            let blob_a = document.createElement('a');
+            blob_a.href = blob_url;
+            blob_a.download = String(i+1) + '.txt';
+            blob_a.click();
+            URL.revokeObjectURL(blob_url);
+            console.log(i+1);
+        }
+        else {
+            xhr.send();
+        }
+    };
+    xhr.send();
+}
