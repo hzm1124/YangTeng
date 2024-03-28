3cb1ab42bb3c5a6d11133e6b36a7967520de57fa
lennon1124 <1210882202@qq.com>
2024-03-28 10:51:23 +0800

    update crawler

--- 02.crawler/15.TurbochargerPros/1_1.Menu_TM-Lennon20240327.js
+++ 02.crawler/15.TurbochargerPros/1_1.Menu_TM-Lennon20240327.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TurbochargerPros爬虫-Menu
 // @namespace    https://github.com/lennon1124/YangTeng
-// @version      2024.01.08
+// @version      2024.03.27
 // @description  Crawler
 // @author       Lennon
 // @match        *://www.turbochargerpros.com/search_result.asp?*
@@ -13,17 +13,27 @@
     'use strict';
 
     window.onload = function() {
+        // parameter
+        let list_param = window.location.href.split('?')[1].split('#')[0].split('&');
+        let param_page = 1;
+        for(let i=0; i<list_param.length; i++) {
+            if(list_param[i].startsWith('page=')) {
+                param_page = parseInt(list_param[i].split('page=')[1]);
+            }
+        }
+
         window.scrollTo(0, document.body.scrollHeight);
         console.log('\n\n\n= = = = = = = = = = = = = = =');
         console.log('Crawler Log 1: Start');
 
         // array
         let array_data = new Array();
-        let list_part = document.querySelectorAll('div.ss-image');
+        let list_part = document.querySelectorAll('div.ss-image>a');
         for(let i=0; i<list_part.length; i++) {
             array_data[i] = {};
+            array_data[i]['Page'] = param_page;
             array_data[i]['No.'] = i + 1;
-            array_data[i]['Url'] = 'https:' + list_part[i].querySelector('a').getAttribute('href').trim();
+            array_data[i]['Url'] = 'https:' + list_part[i].getAttribute('href').trim();
         }
         console.log('Crawler Log 2: Array');
 
@@ -36,7 +46,7 @@
         let blob_url = URL.createObjectURL(blob_data);
         let blob_a = document.createElement('a');
         blob_a.href = blob_url;
-        blob_a.download = window.location.href + '.txt';
+        blob_a.download = String(param_page) + '.txt';
         blob_a.click();
         URL.revokeObjectURL(blob_url);
         console.log('Crawler Log 4: End');
