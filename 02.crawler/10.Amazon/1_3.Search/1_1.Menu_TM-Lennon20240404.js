2dc94a28f09004efdd31488730980cb98daa5192
lennon1124 <1210882202@qq.com>
2024-05-06 14:50:04 +0800

    update crawler

---
+++ 02.crawler/10.Amazon/1_3.Search/1_1.Menu_TM-Lennon20240404.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         Amazon爬虫-Search
+// @namespace    https://github.com/lennon1124/YangTeng
+// @version      2024.04.04
+// @description  Crawler
+// @author       Lennon
+// @match        *://www.amazon.com/*
+// @icon         https://www.amazon.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.onload = function() {
+        // parameter
+        let list_param = window.location.href.split('?')[1].split('&');
+        let param_page = 1;
+        for(let i=0; i<list_param.length; i++) {
+            if(list_param[i].startsWith('page=')) {
+                param_page = parseInt(list_param[i].split('page=')[1]);
+            }
+        }
+
+        window.scrollTo(0, document.body.scrollHeight);
+        console.log('\n\n\n= = = = = = = = = = = = = = =');
+        console.log('Crawler Log 1: Start');
+
+        // array
+        let array_data = new Array();
+        let list_part = document.querySelectorAll('div[data-component-type="s-search-result"]');
+        for(let i=0; i<list_part.length; i++) {
+            array_data[i] = {};
+            array_data[i]['Page'] = param_page;
+            array_data[i]['No.'] = i + 1;
+            array_data[i]['Url'] = 'https://www.amazon.com' + list_part[i].querySelectorAll('a.a-link-normal.s-underline-text.s-underline-link-text.s-link-style.a-text-normal')[0].getAttribute('href').trim();
+        }
+        console.log('Crawler Log 2: Array');
+
+        // json
+        let json_data = JSON.stringify(array_data);
+        console.log('Crawler Log 3: Json');
+
+        // blob
+        let blob_data = new Blob([json_data], {type: 'text/plain'});
+        let blob_url = URL.createObjectURL(blob_data);
+        let blob_a = document.createElement('a');
+        blob_a.href = blob_url;
+        blob_a.download = String(param_page) + '.txt';
+        blob_a.click();
+        URL.revokeObjectURL(blob_url);
+        console.log('Crawler Log 4: End');
+        console.log('= = = = = = = = = = = = = = =\n\n\n');
+
+        // next
+        window.location.href = 'https://www.amazon.com' + document.querySelector('a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator').getAttribute('href').trim();
+    }
+})();
